720cb08bb61df175a9bd72af6918ea1bae2ba089
GeniusOctopus <k.white221@outlook.com>
2023-12-01 16:47:53 +0100

    feat: Day 1 Part 1

---
+++ src/Day1/DayOneController.cs
@@ -0,0 +1,11 @@
+﻿namespace AoC2023.Day1
+{
+    internal class DayOneController
+    {
+        public void Run()
+        {
+            var input = File.ReadAllLines("Day1/input.txt");
+            Console.WriteLine(input.Sum(line => (line.First(char.IsDigit) - '0') * 10 + line.Last(char.IsDigit) - '0'));
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using AoC2023.Day1;
+
+namespace AoC2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new DayOneController().Run();
+        }
+    }
+}
