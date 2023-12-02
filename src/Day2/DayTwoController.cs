011ff368d5d8c09b267cb25b6883b7fbd6173c8e
GeniusOctopus <k.white221@outlook.com>
2023-12-02 15:04:56 +0100

    reafactor: Day 2

--- src/Day2/DayTwoController.cs
+++ src/Day2/DayTwoController.cs
@@ -2,17 +2,27 @@
 {
     internal class DayTwoController
     {
-        private const int MaxRed = 12;
-        private const int MaxGreen = 13;
-        private const int MaxBlue = 14;
         private const string Red = "red";
         private const string Green = "green";
         private const string Blue = "blue";
 
+        private static readonly Dictionary<string, int> _cubes = new()
+        {
+            {Red, 12},
+            {Green, 13},
+            {Blue, 14}
+        };
+
+        private static readonly Dictionary<string, int> _minimumCubes = new()
+        {
+            {Red, 1},
+            {Green, 1},
+            {Blue, 1}
+        };
+
         public static void Run()
         {
             string[] input = File.ReadAllLines("Day2/input.txt");
-
             var sumPart1 = 0;
             var sumPart2 = 0;
 
@@ -20,10 +30,7 @@
             {
                 string[] game = line.Split(':', ';');
                 int gameNumber = int.Parse(game[0].Split(' ')[1]);
-                bool gameIsPossible = true;
-                int minimumRed = 1;
-                int minimumGreen = 1;
-                int minimumBlue = 1;
+                var gameIsPossible = true;
 
                 for (int i = 1; i < game.Length; i++)
                 {
@@ -31,48 +38,24 @@
 
                     foreach (var take in set)
                     {
-                        bool redPossible = true;
-                        bool greenPossible = true;
-                        bool bluePossible = true;
-                        string[] pair = take.Split(' ');
-                        int amount = int.Parse(pair[1]);
+                        string[] pair = take.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                        var amount = int.Parse(pair[0]);
 
-                        switch (pair[2])
-                        {
-                            case Red:
-                                if (amount > MaxRed)
-                                    redPossible = false;
-
-                                if (amount > minimumRed)
-                                    minimumRed = amount;
-                                break;
-                            case Green:
-                                if (amount > MaxGreen)
-                                    greenPossible = false;
-
-                                if (amount > minimumGreen)
-                                    minimumGreen = amount;
-                                break;
-                            case Blue:
-                                if (amount > MaxBlue)
-                                    bluePossible = false;
-
-                                if (amount > minimumBlue)
-                                    minimumBlue = amount;
-                                break;
-                        }
-
-                        if (!(redPossible && greenPossible && bluePossible) && gameIsPossible)
-                        {
+                        if (amount > _cubes[pair[1]])
                             gameIsPossible = false;
-                        }
+
+                        if (amount > _minimumCubes[pair[1]])
+                            _minimumCubes[pair[1]] = amount;
                     }
                 }
 
                 if (gameIsPossible)
                     sumPart1 += gameNumber;
 
-                sumPart2 += minimumRed * minimumGreen * minimumBlue;
+                sumPart2 += _minimumCubes[Red] * _minimumCubes[Green] * _minimumCubes[Blue];
+                _minimumCubes[Red] = 1;
+                _minimumCubes[Green] = 1;
+                _minimumCubes[Blue] = 1;
             }
 
             Console.WriteLine(sumPart1);
