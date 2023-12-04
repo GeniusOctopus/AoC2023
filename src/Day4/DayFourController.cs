8e4591ee38ae957d7fde9aef36028a28fe418b57
GeniusOctopus <k.white221@outlook.com>
2023-12-04 15:05:08 +0100

    feat: Day 4 Part 1

---
+++ src/Day4/DayFourController.cs
@@ -0,0 +1,22 @@
+﻿namespace AoC2023.Day4
+{
+    internal class DayFourController
+    {
+        public static void Run()
+        {
+            string[] input = File.ReadAllLines("Day4/input.txt");
+
+            var sum1 = input
+                .SelectMany((line, n) => line.Split(':').Skip(1)
+                .Select(sequence =>
+                    (sequence.Split('|', StringSplitOptions.RemoveEmptyEntries)
+                        .First().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(x => int.Parse(x)))
+                            .Intersect(
+                    sequence.Split('|', StringSplitOptions.RemoveEmptyEntries).Last().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x))))
+                ).Sum(x => x.Aggregate(0, (acc, n) => acc = x.Any() ? (int)Math.Pow(2, x.Count() - 1) : 0));
+
+            Console.WriteLine(sum1);
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,16 +1,18 @@
 ﻿using AoC2023.Day1;
 using AoC2023.Day2;
 using AoC2023.Day3;
+using AoC2023.Day4;
 
 namespace AoC2023
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            // DayOneController.Run();
+            //DayOneController.Run();
             //DayTwoController.Run();
-            DayThreeController.Run();
+            //DayThreeController.Run();
+            DayFourController.Run();
         }
     }
 }
