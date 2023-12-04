9fd30b5004f22470bfe73f43d6565e3541e441a3
GeniusOctopus <k.white221@outlook.com>
2023-12-04 19:50:01 +0100

    feat: Day 4 Part 2

--- src/Day4/DayFourController.cs
+++ src/Day4/DayFourController.cs
@@ -1,4 +1,6 @@
-﻿namespace AoC2023.Day4
+﻿using System.Collections.Generic;
+
+namespace AoC2023.Day4
 {
     internal class DayFourController
     {
@@ -6,7 +8,7 @@
         {
             string[] input = File.ReadAllLines("Day4/input.txt");
 
-            var sum1 = input
+            var winningNumbers = input
                 .SelectMany((line, n) => line.Split(':').Skip(1)
                 .Select(sequence =>
                     (sequence.Split('|', StringSplitOptions.RemoveEmptyEntries)
@@ -14,9 +16,29 @@
                         .Select(x => int.Parse(x)))
                             .Intersect(
                     sequence.Split('|', StringSplitOptions.RemoveEmptyEntries).Last().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x))))
-                ).Sum(x => x.Aggregate(0, (acc, n) => acc = x.Any() ? (int)Math.Pow(2, x.Count() - 1) : 0));
+                );
 
-            Console.WriteLine(sum1);
+            Console.WriteLine(winningNumbers.Sum(x => x.Aggregate(0, (acc, n) => acc = x.Any() ? (int)Math.Pow(2, x.Count() - 1) : 0)));
+
+            List<(IEnumerable<int> set, int count)> winningSetsWithCount = winningNumbers.Select(x => (x, 1)).ToList();
+
+            for (int i = 0; i < winningSetsWithCount.Count; i++)
+            {
+                for (int j = 0; j < winningSetsWithCount[i].count; j++)
+                {
+                    winningSetsWithCount.Skip(i + 1)
+                            .Take(winningSetsWithCount[i].set.Count()).ToList()
+                                .ForEach(subset =>
+                                {
+                                    var s = winningSetsWithCount.First(x => x.set == subset.set);
+                                    var index = winningSetsWithCount.IndexOf(subset);
+                                    s.count++;
+                                    winningSetsWithCount[index] = s;
+                                });
+                }
+            }
+
+            Console.WriteLine(winningSetsWithCount.Sum(x => x.count));
         }
     }
 }
