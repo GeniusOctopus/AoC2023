4016a53d04737d75db22d91b83ae4153813364e6
GeniusOctopus <k.white221@outlook.com>
2023-12-10 14:34:26 +0100

    Feat: Day 10 visualization

--- src/Day10/DayTenController.cs
+++ src/Day10/DayTenController.cs
@@ -3,13 +3,23 @@
     internal class DayTenController
     {
         static readonly string[] input = File.ReadAllLines("Day10/input.txt");
+        static readonly char[,] map = new char[input.Length, input[0].Length];
         static readonly (int xmin, int xmax) xBound = (0, input[0].Length - 1);
         static readonly (int ymin, int ymax) yBound = (0, input.Length - 1);
 
         public static void Run()
         {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    map[i, j] = '°';
+                }
+            }
+
             (int x, int y) start = input.SelectMany((line, y) => line.Where(x => x == 'S').Select(s => (line.IndexOf(s), y))).First();
             var startPipe = new Pipe(start.x, start.y, Direction.Undefined, PipeTile.Start, null, null, null, null);
+            map[start.y, start.x] = 'S';
 
             startPipe = GetConnectedSuroundingPipes(startPipe, start.x, start.y);
             var currentPipe = startPipe;
@@ -31,6 +41,7 @@
                 currentPipe = GetPipeInDirection(Direction.West, currentPipe.PipeTile, currentPipe.X, currentPipe.Y);
             }
 
+            map[currentPipe.Y, currentPipe.X] = '─';
             var count = 1;
 
             while (currentPipe.PipeTile != PipeTile.Start)
@@ -52,11 +63,40 @@
                     currentPipe = GetPipeInDirection(Direction.West, currentPipe.PipeTile, currentPipe.X, currentPipe.Y);
                 }
 
+                switch (currentPipe.PipeTile)
+                {
+                    case PipeTile.Vertical:
+                        map[currentPipe.Y, currentPipe.X] = '│';
+                        break;
+                    case PipeTile.Horizontal:
+                        map[currentPipe.Y, currentPipe.X] = '─';
+                        break;
+                    case PipeTile.BendNE:
+                        map[currentPipe.Y, currentPipe.X] = '└';
+                        break;
+                    case PipeTile.BendSE:
+                        map[currentPipe.Y, currentPipe.X] = '┌';
+                        break;
+                    case PipeTile.BendSW:
+                        map[currentPipe.Y, currentPipe.X] = '┐';
+                        break;
+                    case PipeTile.BendNW:
+                        map[currentPipe.Y, currentPipe.X] = '┘';
+                        break;
+                }
+
                 count++;
             }
 
-
             Console.WriteLine(count / 2);
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
 
         static Pipe GetPipeInDirection(Direction direction, PipeTile? current, int x, int y)
@@ -134,7 +174,7 @@
                 {
                     '|' => previousPipe != PipeTile.Horizontal && previousPipe != PipeTile.BendSW && previousPipe != PipeTile.BendSE ? PipeTile.Vertical : null,
                     '7' => previousPipe != PipeTile.Horizontal && previousPipe != PipeTile.BendSW && previousPipe != PipeTile.BendSE ? PipeTile.BendSW : null,
-                    'F' => previousPipe != PipeTile.Horizontal && previousPipe != PipeTile.BendSW && previousPipe != PipeTile.BendSE ?  PipeTile.BendSE : null,
+                    'F' => previousPipe != PipeTile.Horizontal && previousPipe != PipeTile.BendSW && previousPipe != PipeTile.BendSE ? PipeTile.BendSE : null,
                     //'S' => PipeTile.Start,
                     _ => null
                 };
@@ -170,7 +210,7 @@
                     '-' => previousPipe != PipeTile.Vertical && previousPipe != PipeTile.BendNE && previousPipe != PipeTile.BendSE ? PipeTile.Horizontal : null,
                     'L' => previousPipe != PipeTile.Vertical && previousPipe != PipeTile.BendNE && previousPipe != PipeTile.BendSE ? PipeTile.BendNE : null,
                     'F' => previousPipe != PipeTile.Vertical && previousPipe != PipeTile.BendNE && previousPipe != PipeTile.BendSE ? PipeTile.BendSE : null,
-                    'S' => previousPipe != PipeTile.Vertical && previousPipe != PipeTile.BendNE && previousPipe != PipeTile.BendSE ?  PipeTile.Start : null,
+                    'S' => previousPipe != PipeTile.Vertical && previousPipe != PipeTile.BendNE && previousPipe != PipeTile.BendSE ? PipeTile.Start : null,
                     _ => null
                 };
             }
